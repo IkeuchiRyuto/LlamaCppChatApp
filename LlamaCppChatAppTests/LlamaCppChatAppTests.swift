bcb9da46b64170db6c6142c850b0fe276ecc3a74
ikeuchi.ryuto <ikeuchi.ryuto@headwaters.co.jp>
2025-03-15 16:01:44 +0900

    first commit

---
+++ LlamaCppChatAppTests/LlamaCppChatAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  LlamaCppChatAppTests.swift
+//  LlamaCppChatAppTests
+//
+//  Created by ikeuchi.ryuto on 2025/03/15.
+//
+
+import Testing
+
+struct LlamaCppChatAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
