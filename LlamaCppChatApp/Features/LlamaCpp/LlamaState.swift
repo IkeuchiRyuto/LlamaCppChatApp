1650b1b1f2e077ef562738035988df1adc9da7ac
ikeuchi.ryuto <ikeuchi.ryuto@headwaters.co.jp>
2025-03-16 17:07:57 +0900

    モデルのロードやダウンロードのロジックを全てLLamaStateに移す

--- LlamaCppChatApp/Features/LlamaCpp/LlamaState.swift
+++ LlamaCppChatApp/Features/LlamaCpp/LlamaState.swift
@@ -15,19 +15,30 @@ struct Model: Identifiable {
     var status: String?
 }
 
+enum Status: String {
+    case loading
+    case loaded
+    case downloading
+    case running
+    case finished
+}
+
 @MainActor
 class LlamaState: ObservableObject {
-    @Published var messageLog = ""
-    @Published var cacheCleared = false
+    @Published var output = ""
+    @Published var batchOutput: String = ""
+    @Published var deviceStatus = ""
     @Published var downloadedModels: [Model] = []
     @Published var undownloadedModels: [Model] = []
+    @Published var status = Status.loading
+    @Published var progressTime: Double = 0
+
     let NS_PER_S = 1_000_000_000.0
 
     private var llamaContext: LlamaContext?
-    private var defaultModelUrl: URL? {
-        Bundle.main.url(forResource: "ggml-model", withExtension: "gguf", subdirectory: "models")
-        // Bundle.main.url(forResource: "llama-2-7b-chat", withExtension: "Q2_K.gguf", subdirectory: "models")
-    }
+    private var selectedModel: Model?
+    private var downloadTask: URLSessionDownloadTask?
+    private var observation: NSKeyValueObservation?
 
     init() {
         loadModelsFromDisk()
@@ -37,10 +48,17 @@ class LlamaState: ObservableObject {
     private func loadModelsFromDisk() {
         do {
             let documentsURL = getDocumentsDirectory()
-            let modelURLs = try FileManager.default.contentsOfDirectory(at: documentsURL, includingPropertiesForKeys: nil, options: [.skipsHiddenFiles, .skipsSubdirectoryDescendants])
+            let modelURLs = try FileManager.default.contentsOfDirectory(
+                at: documentsURL, includingPropertiesForKeys: nil,
+                options: [.skipsHiddenFiles, .skipsSubdirectoryDescendants])
             for modelURL in modelURLs {
-                let modelName = modelURL.deletingPathExtension().lastPathComponent
-                downloadedModels.append(Model(name: modelName, url: "", filename: modelURL.lastPathComponent, status: "downloaded"))
+                let modelName = modelURL.deletingPathExtension()
+                    .lastPathComponent
+                downloadedModels.append(
+                    Model(
+                        name: modelName, url: "",
+                        filename: modelURL.lastPathComponent,
+                        status: "downloaded"))
             }
         } catch {
             print("Error loading models from disk: \(error)")
@@ -48,14 +66,9 @@ class LlamaState: ObservableObject {
     }
 
     private func loadDefaultModels() {
-        do {
-            try loadModel(modelUrl: defaultModelUrl)
-        } catch {
-            messageLog += "Error!\n"
-        }
-
         for model in defaultModels {
-            let fileURL = getDocumentsDirectory().appendingPathComponent(model.filename)
+            let fileURL = getDocumentsDirectory().appendingPathComponent(
+                model.filename)
             if FileManager.default.fileExists(atPath: fileURL.path) {
 
             } else {
@@ -67,9 +80,11 @@ class LlamaState: ObservableObject {
     }
 
     func getDocumentsDirectory() -> URL {
-        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let paths = FileManager.default.urls(
+            for: .documentDirectory, in: .userDomainMask)
         return paths[0]
     }
+
     private let defaultModels: [Model] = [
         Model(
             name: "Phi-4-mini-q8(Q8_0, 4.06 GiB)",
@@ -89,24 +104,104 @@ class LlamaState: ObservableObject {
             filename: "Phi-4-mini-instruct.Q16.gguf", status: "download"
         ),
     ]
+
     func loadModel(modelUrl: URL?) throws {
         if let modelUrl {
-            messageLog += "Loading model...\n"
-            llamaContext = try LlamaContext.create_context(path: modelUrl.path())
-            messageLog += "Loaded model \(modelUrl.lastPathComponent)\n"
+            deviceStatus += "Loading model...\n"
+            llamaContext = try LlamaContext.create_context(
+                path: modelUrl.path())
+            deviceStatus += "Loaded model \(modelUrl.lastPathComponent)\n"
 
             // Assuming that the model is successfully loaded, update the downloaded models
-            updateDownloadedModels(modelName: modelUrl.lastPathComponent, status: "downloaded")
+            updateDownloadedModels(
+                modelName: modelUrl.lastPathComponent, status: "downloaded")
         } else {
-            messageLog += "Load a model from the list below\n"
+            deviceStatus += "Load a model from the list below\n"
         }
     }
-
 
     private func updateDownloadedModels(modelName: String, status: String) {
         undownloadedModels.removeAll { $0.name == modelName }
     }
 
+    private func getFileURL(filename: String) -> URL {
+        FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[
+            0
+        ].appendingPathComponent(filename)
+    }
+
+    private func download() {
+        guard let selectedModel else { return }
+        guard let url = URL(string: selectedModel.url) else { return }
+        let fileURL = getFileURL(filename: selectedModel.filename)
+
+        status = Status.downloading
+        downloadTask = URLSession.shared.downloadTask(with: url) {
+            temporaryURL, response, error in
+            if let error = error {
+                print("Error: \(error.localizedDescription)")
+                return
+            }
+
+            guard let response = response as? HTTPURLResponse,
+                (200...299).contains(response.statusCode)
+            else {
+                print("Server error!")
+                return
+            }
+
+            do {
+                if let temporaryURL = temporaryURL {
+                    try FileManager.default.copyItem(
+                        at: temporaryURL, to: fileURL)
+                    Task { @MainActor in
+                        print(
+                            "Writing to \(selectedModel.filename) completed"
+                        )
+
+                        let model = Model(
+                            name: selectedModel.name,
+                            url: selectedModel.url,
+                            filename: selectedModel.filename,
+                            status: "downloaded")
+                        self.downloadedModels.append(model)
+                        self.status = Status.loaded
+                    }
+                }
+            } catch let err {
+                print("Error: \(err.localizedDescription)")
+            }
+        }
+
+        observation = downloadTask?.progress.observe(\.fractionCompleted) {
+            progress, _ in
+            Task { @MainActor in
+                self.progressTime = progress.fractionCompleted
+                print(self.progressTime)
+            }
+        }
+
+        downloadTask?.resume()
+    }
+
+    func load(modelName: String) {
+        selectedModel = defaultModels.first(where: {
+            $0.filename == modelName
+        })
+
+        let fileURL = getFileURL(filename: selectedModel!.filename)
+
+        if !FileManager.default.fileExists(atPath: fileURL.path) {
+            download()
+            return
+        }
+
+        do {
+            try loadModel(modelUrl: fileURL)
+        } catch let err {
+            print("Error: \(err.localizedDescription)")
+        }
+    }
 
     func complete(text: String) async {
         guard let llamaContext else {
@@ -117,67 +212,68 @@ class LlamaState: ObservableObject {
         await llamaContext.completion_init(text: text)
         let t_heat_end = DispatchTime.now().uptimeNanoseconds
         let t_heat = Double(t_heat_end - t_start) / NS_PER_S
-        messageLog += "\(text)"
+        output = ""
+        batchOutput = ""
 
         Task.detached {
             while await !llamaContext.is_done {
                 let result = await llamaContext.completion_loop()
                 await MainActor.run {
-                    self.messageLog += "\(result)"
+                    self.output += "\(result)"
                 }
             }
 
             let t_end = DispatchTime.now().uptimeNanoseconds
             let t_generation = Double(t_end - t_heat_end) / self.NS_PER_S
-            let tokens_per_second = Double(await llamaContext.n_len) / t_generation
+            let tokens_per_second =
+                Double(await llamaContext.n_len) / t_generation
 
             await llamaContext.clear()
             await MainActor.run {
-                self.messageLog += """
-                    \n
-                    Done
-                    Heat up took \(t_heat)s
-                    Generated \(tokens_per_second) t/s\n
+                self.batchOutput = self.output
+                self.deviceStatus += """
+                    Heat up took \(t_heat)s Generated \(tokens_per_second) t/s\n
                     """
             }
         }
     }
 
-    func bench() async {
-        guard let llamaContext else {
-            return
-        }
-
-        messageLog += "\n"
-        messageLog += "Running benchmark...\n"
-        messageLog += "Model info: "
-        messageLog += await llamaContext.model_info() + "\n"
-
-        let t_start = DispatchTime.now().uptimeNanoseconds
-        let _ = await llamaContext.bench(pp: 8, tg: 4, pl: 1) // heat up
-        let t_end = DispatchTime.now().uptimeNanoseconds
-
-        let t_heat = Double(t_end - t_start) / NS_PER_S
-        messageLog += "Heat up time: \(t_heat) seconds, please wait...\n"
-
-        // if more than 5 seconds, then we're probably running on a slow device
-        if t_heat > 5.0 {
-            messageLog += "Heat up time is too long, aborting benchmark\n"
-            return
-        }
-
-        let result = await llamaContext.bench(pp: 512, tg: 128, pl: 1, nr: 3)
-
-        messageLog += "\(result)"
-        messageLog += "\n"
-    }
-
-    func clear() async {
-        guard let llamaContext else {
-            return
-        }
-
-        await llamaContext.clear()
-        messageLog = ""
-    }
+    //    func bench() async {
+    //        guard let llamaContext else {
+    //            return
+    //        }
+    //
+    //        messageLog += "\n"
+    //        messageLog += "Running benchmark...\n"
+    //        messageLog += "Model info: "
+    //        messageLog += await llamaContext.model_info() + "\n"
+    //
+    //        let t_start = DispatchTime.now().uptimeNanoseconds
+    //        let _ = await llamaContext.bench(pp: 8, tg: 4, pl: 1)  // heat up
+    //        let t_end = DispatchTime.now().uptimeNanoseconds
+    //
+    //        let t_heat = Double(t_end - t_start) / NS_PER_S
+    //        messageLog += "Heat up time: \(t_heat) seconds, please wait...\n"
+    //
+    //        // if more than 5 seconds, then we're probably running on a slow device
+    //        if t_heat > 5.0 {
+    //            messageLog += "Heat up time is too long, aborting benchmark\n"
+    //            return
+    //        }
+    //
+    //        let result = await llamaContext.bench(pp: 512, tg: 128, pl: 1, nr: 3)
+    //
+    //        messageLog += "\(result)"
+    //        messageLog += "\n"
+    //    }
+    //
+    //    func clear() async {
+    //        guard let llamaContext else {
+    //            return
+    //        }
+    //
+    //        await llamaContext.clear()
+    //        messageLog = ""
+    //        deviceStatus = ""
+    //    }
 }
